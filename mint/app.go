fa75eb0958b350138c766dcdcc3020bfd2cfa080
422145328@qq.com <422145328@qq.com>
2024-02-11 08:29:00 +0800

    update  app use  deployment

--- mint/app.go
+++ mint/app.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 	chain "github.com/wetee-dao/go-sdk"
 	gtype "github.com/wetee-dao/go-sdk/gen/types"
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -46,9 +47,21 @@ func (m *Minter) DoWithAppState(ctx *context.Context, c ContractStateWrap, stage
 		name := util.GetWorkTypeStr(workId) + "-" + fmt.Sprint(workId.Id)
 		nameSpace := AccountToAddress(c.ContractState.User[:])
 
+		// 获取pod信息
+		// Get pod information
+		clientset := m.K8sClient
+		pods, err := clientset.CoreV1().Pods(nameSpace).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: "app=" + name,
+		})
+		fmt.Println("pods: ", pods.Items[0].Name)
+		if err != nil {
+			util.LogWithRed("getMetricInfo", err)
+			return err
+		}
+
 		// 获取log和硬件资源使用量
 		// Get log and hardware resource usage
-		logs, crs, err := m.getMetricInfo(*ctx, workId, nameSpace, name, uint64(head.Number)-state.BlockNumber)
+		logs, crs, err := m.getMetricInfo(*ctx, workId, nameSpace, pods.Items[0].Name, uint64(head.Number)-state.BlockNumber)
 		if err != nil {
 			util.LogWithRed("getMetricInfo", err)
 			return err
@@ -71,6 +84,7 @@ func (m *Minter) DoWithAppState(ctx *context.Context, c ContractStateWrap, stage
 		}
 
 		// 初始化worker对象
+		// init worker object
 		worker := chain.Worker{
 			Client: m.ChainClient,
 			Signer: Signer,
@@ -94,9 +108,9 @@ func (m *Minter) DoWithAppState(ctx *context.Context, c ContractStateWrap, stage
 
 // checkAppStatus check app status
 // 校对应用状态
-func (m *Minter) CheckAppStatus(ctx *context.Context, state ContractStateWrap) (*v1.Pod, error) {
+func (m *Minter) CheckAppStatus(ctx *context.Context, state ContractStateWrap) (*appsv1.Deployment, error) {
 	address := AccountToAddress(state.ContractState.User[:])
-	nameSpace := m.K8sClient.CoreV1().Pods(address)
+	nameSpace := m.K8sClient.AppsV1().Deployments(address)
 	workId := state.ContractState.WorkId
 	name := util.GetWorkTypeStr(workId) + "-" + fmt.Sprint(workId.Id)
 
@@ -106,7 +120,7 @@ func (m *Minter) CheckAppStatus(ctx *context.Context, state ContractStateWrap) (
 		return nil, errors.New("app stop")
 	}
 
-	pod, err := nameSpace.Get(*ctx, name, metav1.GetOptions{})
+	deployment, err := nameSpace.Get(*ctx, name, metav1.GetOptions{})
 	version := state.Version
 	if err != nil {
 		if !strings.Contains(err.Error(), "not found") {
@@ -122,13 +136,13 @@ func (m *Minter) CheckAppStatus(ctx *context.Context, state ContractStateWrap) (
 		if err != nil {
 			return nil, err
 		}
-		pod, err = nameSpace.Get(*ctx, name, metav1.GetOptions{})
+		deployment, err = nameSpace.Get(*ctx, name, metav1.GetOptions{})
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return pod, err
+	return deployment, err
 }
 
 // CreateOrUpdateApp create or update app
@@ -140,7 +154,7 @@ func (m *Minter) CreateApp(ctx *context.Context, user []byte, workId gtype.WorkI
 		return errc
 	}
 
-	nameSpace := m.K8sClient.CoreV1().Pods(saddress)
+	nameSpace := m.K8sClient.AppsV1().Deployments(saddress)
 	name := util.GetWorkTypeStr(workId) + "-" + fmt.Sprint(workId.Id)
 
 	err := dao.SetSecrets(workId, &dao.Secrets{
@@ -158,35 +172,45 @@ func (m *Minter) CreateApp(ctx *context.Context, user []byte, workId gtype.WorkI
 	}
 
 	resource.NewMilliQuantity(int64(app.Cr.Mem)*1024*1024, resource.BinarySI)
-	pod := &v1.Pod{
-		TypeMeta: metav1.TypeMeta{Kind: "App", APIVersion: "v1"},
+
+	deployment := appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        name,
 			Annotations: map[string]string{"version": fmt.Sprint(version)},
 		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name:  "c1",
-					Image: string(app.Image),
-					Ports: []v1.ContainerPort{
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": name},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": name},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
 						{
-							Name:          string(app.Name) + "0",
-							ContainerPort: int32(app.Port[0]),
-							Protocol:      "TCP",
-						},
-					},
-					Env: envs,
-					Resources: v1.ResourceRequirements{
-						Limits: v1.ResourceList{
-							// v1.ResourceCPU:                 resource.MustParse(fmt.Sprint(app.Cr.Cpu) + "m"),
-							// v1.ResourceMemory:              resource.MustParse(fmt.Sprint(app.Cr.Mem) + "M"),
-							"alibabacloud.com/sgx_epc_MiB": *resource.NewQuantity(int64(20), resource.DecimalExponent),
-						},
-						Requests: v1.ResourceList{
-							// v1.ResourceCPU:                 resource.MustParse(fmt.Sprint(app.Cr.Cpu) + "m"),
-							// v1.ResourceMemory:              resource.MustParse(fmt.Sprint(app.Cr.Mem) + "M"),
-							"alibabacloud.com/sgx_epc_MiB": *resource.NewQuantity(int64(20), resource.DecimalExponent),
+							Name:  "c1",
+							Image: string(app.Image),
+							Ports: []v1.ContainerPort{
+								{
+									Name:          string(app.Name) + "0",
+									ContainerPort: int32(app.Port[0]),
+									Protocol:      "TCP",
+								},
+							},
+							Env: envs,
+							Resources: v1.ResourceRequirements{
+								Limits: v1.ResourceList{
+									// v1.ResourceCPU:                 resource.MustParse(fmt.Sprint(app.Cr.Cpu) + "m"),
+									// v1.ResourceMemory:              resource.MustParse(fmt.Sprint(app.Cr.Mem) + "M"),
+									"alibabacloud.com/sgx_epc_MiB": *resource.NewQuantity(int64(20), resource.DecimalExponent),
+								},
+								Requests: v1.ResourceList{
+									// v1.ResourceCPU:                 resource.MustParse(fmt.Sprint(app.Cr.Cpu) + "m"),
+									// v1.ResourceMemory:              resource.MustParse(fmt.Sprint(app.Cr.Mem) + "M"),
+									"alibabacloud.com/sgx_epc_MiB": *resource.NewQuantity(int64(20), resource.DecimalExponent),
+								},
+							},
 						},
 					},
 				},
@@ -197,7 +221,7 @@ func (m *Minter) CreateApp(ctx *context.Context, user []byte, workId gtype.WorkI
 	bt, _ := json.Marshal(20)
 	fmt.Println(string(bt))
 
-	_, err = nameSpace.Create(*ctx, pod, metav1.CreateOptions{})
+	_, err = nameSpace.Create(*ctx, &deployment, metav1.CreateOptions{})
 	fmt.Println("================================================= Create", err)
 
 	return err
@@ -209,10 +233,20 @@ func (m *Minter) UpdateApp(ctx *context.Context, user []byte, workId gtype.WorkI
 		return nil
 	}
 
-	// 停止镜像再部署
-	m.StopApp(workId)
-	err := m.CreateApp(ctx, user, workId, app, envs, version)
-	fmt.Println("================================================= Update", err)
+	saddress := AccountToAddress(user)
+	nameSpace := m.K8sClient.AppsV1().Deployments(saddress)
+	name := util.GetWorkTypeStr(workId) + "-" + fmt.Sprint(workId.Id)
+
+	existing, err := nameSpace.Get(*ctx, name, metav1.GetOptions{})
+	if err == nil {
+		existing.ObjectMeta.Annotations = map[string]string{
+			"version": fmt.Sprint(version),
+		}
+		existing.Spec.Template.Spec.Containers[0].Image = string(app.Image)
+		existing.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort = int32(app.Port[0])
+		_, err = nameSpace.Update(*ctx, existing, metav1.UpdateOptions{})
+		fmt.Println("================================================= Update", err)
+	}
 
 	return err
 }
@@ -228,7 +262,7 @@ func (m *Minter) StopApp(workId gtype.WorkId) error {
 
 	saddress := AccountToAddress(user[:])
 
-	nameSpace := m.K8sClient.CoreV1().Pods(saddress)
+	nameSpace := m.K8sClient.AppsV1().Deployments(saddress)
 	name := util.GetWorkTypeStr(workId) + "-" + fmt.Sprint(workId.Id)
 	return nameSpace.Delete(ctx, name, metav1.DeleteOptions{})
 }
